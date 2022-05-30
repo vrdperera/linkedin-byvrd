e6f2a66c497108a20bbfda288b1e6f57f6f0b6ad
Vrd Perera <vrdperera@gmail.com>
2022-05-30 20:18:55 +0530

    modified

--- pages/home.js
+++ pages/home.js
@@ -20,15 +20,29 @@ function Home() {
           />
         </div>
 
-        <div className="flex items-center  sm:divide-x divide-gray-400 ">
+        <div className="flex items-center  sm:divide-x divide-blue-400  gap-3 ">
           <div className="hidden sm:flex space-x-8">
             <HeaderLinks Icon={ExploreIcon} text="Explore" />
             <HeaderLinks Icon={EngineeringIcon} text="Engineering" />
             <HeaderLinks Icon={FingerprintIcon} text="Fingerprint" />
             <HeaderLinks Icon={CloudDoneIcon} text="Cloud" />
           </div>
+
+          <div className="pl-4">
+            <button className="text-blue-700 font-semibold  border border-gray-400  px-5  py-1.5 rounded-full hover:border-2">
+              Sign in
+            </button>
+          </div>
         </div>
       </header>
+
+      <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto ">
+        <div className="space-y-6  xl:space-y-10">
+          <h1 className="text-3xl md:text-5xl">
+            Welcome to your professional community
+          </h1>
+        </div>
+      </main>
     </div>
   );
 }
