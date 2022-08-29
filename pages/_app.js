f140b0decb1b96e7e4fac893129649997bcb466f
Vrd Perera <vrdperera@gmail.com>
2022-08-29 14:30:15 +0530

    refactor

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps: { session, pageProps } }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class">
