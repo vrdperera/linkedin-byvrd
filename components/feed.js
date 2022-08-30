ceab5f2210ebf1a4c697bdd83f10d3cb321bd87e
Vrd Perera <vrdperera@gmail.com>
2022-08-30 18:34:39 +0530

    add some feed functionality and test with client side data fetching

--- components/feed.js
+++ components/feed.js
@@ -1,3 +1,26 @@
+import { useEffect, useState } from 'react';
+import Input from './input';
+
 export default function Feed() {
-  return <div>feed</div>;
+  const [realTimePosts, setRealTimePosts] = useState([]);
+  console.log(realTimePosts);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/posts', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const responseData = await response.json();
+      setRealTimePosts(responseData);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="space-y-6 pb-24 max-w-lg">
+      <Input />
+      {/* posts  */}
+    </div>
+  );
 }
