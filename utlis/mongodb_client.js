d2098218acb70e59c08c11d82ded397d9641c990
Vrd Perera <vrdperera@gmail.com>
2022-08-31 15:06:27 +0530

    big refactor, previous connection really messd the realtime posting.

--- utlis/mongodb_client.js
+++ utlis/mongodb_client.js
@@ -1,47 +1,37 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
-// get environment variables for database uri and database name
 let uri = process.env.MONGODB_URI;
 let dbName = process.env.MONGODB_DB;
 
-// create cache variables, we can cache our connection
 let cachedClient = null;
 let cachedDb = null;
 
-// database connection function
+if (!uri) {
+  throw new Error(
+    'Please define the MONGO_CONNECTION environment variable inside .env'
+  );
+}
+
+if (!dbName) {
+  throw new Error(
+    'Please define the MONGO_DB environment variable inside .env'
+  );
+}
+
 export async function connectToDatabase() {
-  // Create a new MongoClient
-  const client = new MongoClient(uri, {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = await MongoClient.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  {
-    // check for database connection string and db name
-    if (!uri || !dbName) {
-      throw new Error('No URI available for MongoDB connection');
-    }
 
-    // if have cached use it
-    if (cachedClient && cachedDb) {
-      return { cachedClient, cachedDb };
-    }
-  }
+  const db = await client.db(dbName);
 
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Establish and verify connection
-    const db = await client.db(dbName);
-    console.log('Connected successfully to server');
+  cachedClient = client;
+  cachedDb = db;
 
-    cachedClient = client;
-    cachedDb = db;
-
-    return { client, db };
-  } catch (err) {
-    throw new Error(err?.message ?? err.toString());
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+  return { client, db };
 }
