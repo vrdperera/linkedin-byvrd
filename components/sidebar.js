42986ec62b7ef9e32354d42e8664eda2868caef3
Vrd Perera <vrdperera@gmail.com>
2022-08-31 23:09:29 +0530

    complete sidebar component

--- components/sidebar.js
+++ components/sidebar.js
@@ -62,7 +62,7 @@ function Sidebar() {
         </div>
       </div>
       {/* Bottom */}
-      <div className="hidden md:flex bg-white dark:bg-[#1D2226] text-black/70 dark:text-white/75 rounded-lg overflow-hidden flex-col space-y-2 pt-2.5 sticky top-20 border border-gray-300 dark:border-none">
+      <div className="hidden md:flex bg-white dark:bg-[#1D2226] text-black/70 dark:text-white/75 rounded-lg overflow-hidden flex-col space-y-2 pt-2.5 sticky top-[104px] border border-gray-300 dark:border-none">
         <p className="sidebarLink">Groups</p>
         <div className="flex items-center justify-between">
           <p className="sidebarLink">Events</p>
