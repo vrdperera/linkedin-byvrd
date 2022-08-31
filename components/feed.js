1d066cb5e6088478f11c130a54e872f067ab27a0
Vrd Perera <vrdperera@gmail.com>
2022-08-31 21:04:36 +0530

    complete the feed component

--- components/feed.js
+++ components/feed.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { handlePostState, useSSRPostsState } from '../atoms/postAtom';
 import Input from './input';
+import Post from './post';
 
 export default function Feed({ posts }) {
   const [realTimePosts, setRealTimePosts] = useState([]);
   const [handlePost, setHandlePost] = useRecoilState(handlePostState);
   const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
-  console.log(realTimePosts);
+  console.log('realTimePosts', realTimePosts);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -21,12 +22,17 @@ export default function Feed({ posts }) {
       setUseSSRPosts(false);
     };
     fetchPosts();
-  }, [handlePost]);
+  }, [handlePost, setHandlePost, setUseSSRPosts]);
 
   return (
     <div className="space-y-6 pb-24 max-w-lg">
       <Input />
       {/* posts  */}
+      {!useSSRPosts
+        ? Array.from(realTimePosts).map((post) => (
+            <Post key={post._id} post={post} />
+          ))
+        : posts.map((post) => <Post key={post._id} post={post} />)}
     </div>
   );
 }
